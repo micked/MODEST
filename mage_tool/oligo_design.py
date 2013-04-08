6287774373decd066fb3a039904cacf1fedb1d6f
Michael Schantz Klausen <sch@ntz.nu>
2013-04-08 21:05:16 +0200

    Sequence/wobble experiment

--- mage_tool/oligo_design.py
+++ mage_tool/oligo_design.py
@@ -4,11 +4,17 @@
 Module for designing DNA oligos from mutation objects
 """
 
+from __future__ import print_function
+
 import logging
 from copy import deepcopy
 
-from helpers import reverse_complement
 import ViennaRNA
+from helpers import valid_dna
+from helpers import valid_rna
+from helpers import valid_dgn
+from helpers import reverse_complement
+from helpers import degenerate_nucleotides
 
 #Define a log
 log = logging.getLogger("MODEST.oligo")
@@ -381,6 +387,230 @@ class Gene:
     def copy(self):
         """Return a copy"""
         return deepcopy(self)
+
+
+class Sequence:
+    """Sequence object"""
+    def __init__(self, seq):
+        seq = str(seq).upper()
+        self.wbunits = list()
+
+        if valid_dna(seq):
+            self.typ = 1
+            self.type = "DNA"
+        # elif valid_rna(seq):
+        #     self.typ = 2
+        #     self.type = "RNA"
+        else:
+            raise ValueError("Invalid sequence: {}".format(seq))
+
+        self.seql = dict()
+        for i, n in enumerate(seq):
+            self.seql[(i, 0)] = [n, n]
+
+        self.mutations = dict()
+        self.positions = sorted(self.seql.keys())
+
+    def add_wobble(self, wbseq, pos):
+        """Add a wobble unit"""
+        wbl = WobbleSequence(wbseq, pos)
+        if self.test_wobble(wbl):
+            self.wbunits.append(wbl)
+            self.wbunits.sort(key=lambda wbl: wbl.start)
+        else:
+            raise ValueError("Wobble does not match seq: {}".format(wbseq))
+
+    def test_wobble(self, wbl):
+        """Test a single wobble sequence"""
+        st, nd = wbl.range()
+        for i, n in enumerate(self[st:nd], st):
+            if n not in wbl.possible[i]:
+                return False
+        return True
+
+    def __len__(self):
+        return len(self.seql)
+
+    def __getitem__(self, slc):
+        if type(slc) is slice:            
+            return self.get_seq()[slc]
+        elif type(slc) is int:
+            return self.seql[(slc, 0)][0]
+        elif type(slc) is tuple:
+            return self.seql[slc][0]
+        else:
+            raise ValueError("Invalid key: {}/type: {}. Allowed: slice, int, tuple.".format(slc, type(slc)))
+
+    def __str__(self):
+        return self.seq
+
+    def copy(self):
+        return deepcopy(self)
+
+    def pprint(self, w_numbers=False):
+        """Pretty print"""
+        start = min(self.wbunits[0].start, 0) * -1 if self.wbunits else 0
+        #Original sequence
+        org_seq = self.get_original_seq()
+        if w_numbers:
+            print("     " + " " * start, end="")
+            i = 0
+            for j, n in enumerate(org_seq):
+                if j % 5 == 0: print("{:<5}".format(i), end="")
+                if n != "-": i += 1
+            print()
+
+        print("ORG: " + " " * start, end="")
+        print(org_seq)
+
+        #Mutated sequence
+        mut_seq = self.get_mutated_seq()
+        if w_numbers:
+            print("     " + " " * start, end="")
+            i = 0
+            for j, n in enumerate(mut_seq):
+                if j % 5 == 0: print("{:<5}".format(i), end="")
+                if n != "-": i += 1
+            print()
+
+        print("MUT: " + " " * start, end="")
+        print(mut_seq)
+
+        #Wobble sequences
+        if self.wbunits:
+            print("WBL: " + " " * max(self.wbunits[0].start, 0), end="")
+            last_end = 0
+            for wbl in self.wbunits:
+                diff = max(wbl.start - last_end, 0)
+                print(" "*diff, end="")
+                print(wbl, end="")
+                last_end = wbl.end
+            print()
+
+    def mutate(self, pos, n):
+        """Mutate pos to n"""
+        if len(n) != 1:
+            raise ValueError("Only point mutations allowed in mutate() (len(n) != 0)")
+
+        pos, abs_pos = self.get_abs_pos(pos)
+        if pos is None: return False
+
+        for wbl in self.wbunits:
+            if n not in wbl[abs_pos]:
+                return False
+
+        if self.seql[pos][0] == n:
+            return False
+
+        self.seql[pos][0] = n
+
+        if self.seql[pos][1] == n:
+            del(self.mutations[pos])
+        else:
+            self.mutations[pos] = n
+
+        return True
+
+    def delete(self, pos):
+        """Delete pos"""
+        pos, abs_pos = self.get_abs_pos(pos)
+        if pos is None: return False
+
+        #Check whether insert is inside a wobble
+        for wbl in self.wbunits:
+            if abs_pos in wbl:
+                return False
+
+        #Already deleted
+        if self.seql[pos][0] == "-":
+            return False
+
+        self.seql[pos][0] = "-"
+        self.mutations[pos] = "deletion"
+        del(self.positions[abs_pos])
+        #TODO: insert stuff
+
+        for wbl in self.wbunits:
+            if abs_pos < wbl:
+                wbl.offset -= 1
+
+        return True
+
+    def get_abs_pos(self, pos):
+        """Get absolute and index position from unknown pos"""
+        if type(pos) is int:
+            abs_pos = pos
+            try:
+                pos = self.positions[pos]
+            except IndexError:
+                return None, None
+        elif type(pos) is tuple:
+            try:
+                abs_pos = self.positions.index(pos)
+            except ValueError:
+                return None, None
+        else:
+            raise ValueError("pos can only be int or tuple in mutation(). Found: {}".format(type(pos)))
+
+        return pos, abs_pos
+
+
+    def get_seq(self):
+        """Set seq string"""
+        return "".join([self.seql[pos][0] for pos in sorted(self.seql) if self.seql[pos][0] != "-"])
+        #self.positions = [x for x in sorted(self.seql) if self.seql[x][0] != "-"]
+
+    def get_original_seq(self):
+        """Original sequence including gaps"""
+        return "".join([self.seql[pos][1] for pos in sorted(self.seql)])
+
+    def get_mutated_seq(self):
+        """Mutated sequence including gaps"""
+        return "".join([self.seql[pos][0] for pos in sorted(self.seql)])
+
+
+class WobbleSequence:
+    """Wobble sequence object"""
+
+    nucleotides = set(["A", "T", "G", "C"])
+
+    def __init__(self, wbseq, pos):
+        self.seq = str(wbseq).upper()
+        if not valid_dgn(self.seq):
+            raise ValueError("Invalid wobble sequence: {}".format(wbseq))
+
+        self.start = pos
+        self.end   = pos + len(wbseq)
+
+        self.offset = 0
+
+        self.possible = dict()
+        for i, n in enumerate(self.seq, pos):
+            if n in ["A", "T", "G", "C"]:
+                self.possible[i] = [n]
+            else:    
+                self.possible[i] = degenerate_nucleotides[n]
+
+    def range(self):
+        return (max(self.start + self.offset, 0), self.end + self.offset)
+
+    def __str__(self):
+        return self.seq
+
+    def __repr__(self):
+        return "wobble({}..{}: {})".format(self.start, self.end, self.seq)
+
+    def __getitem__(self, i):
+        if self.start + self.offset <= i < self.end + self.offset:
+            return self.possible[i - self.offset]
+        else:
+            return self.nucleotides
+
+    def __contains__(self, i):
+        return self.start + self.offset <= i < self.end + self.offset
+
+    def __gt__(self, i):
+        return i < self.start + self.offset
 
 
 class MutationError(ValueError): pass
