462ada5cd3c2b07b74ae59461de2241daeb0d5ef
Michael Schantz <sch@ntz.nu>
2013-06-13 09:19:35 +0200

    rc module

---
+++ mage_tool/run_control.py
@@ -0,0 +1,34 @@
+
+"""
+Configuration/run control module for mage_tool
+
+This module contains a dictionary containing the module-wide configuration.
+"""
+
+import yaml
+import os.path
+
+CONF = {#Oligo options
+        "oligo_length": 90,
+
+        #Program options
+        "processes": None}
+
+def load_rcfile(filepath):
+    """Load a mage_tool configuration.
+
+    Configuration must be in YAML format.
+
+    """
+    #print "trying", filepath
+    if os.path.isfile(filepath):
+        #print "parsing", filepath
+        with open(filepath, "r") as f:
+            CONF.update(yaml.safe_load(f))
+
+
+DEFAULT_RC_FILES = ["~/.conf/mage_tool.rc", "./mage_tool.rc",]
+
+for rcfile in DEFAULT_RC_FILES:
+    load_rcfile(rcfile)
+
